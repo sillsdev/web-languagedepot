22e19674dec2a68e623863716cb750c5a752eccd
Cambell <cambell.prince@gmail.com>
2014-09-23 22:15:07 +0700

    Finished conversion to nvd3 charts

--- src/app-ng/stats/partials/projects.js
+++ src/app-ng/stats/partials/projects.js
@@ -33,6 +33,8 @@ angular.module('sa.projects', [
 
         d3.select(".chartPie").datum(data).transition().duration(1200).call(chart);
 
+        nv.utils.windowResize(chart.update);
+
         return chart;
       });
 
@@ -40,173 +42,97 @@ angular.module('sa.projects', [
 
   }])
   .controller('PublicProjectsCtrl', [ '$scope', 'PublicProjectService', function($scope, PublicProjectService) {
-
-    var margin = {top: 20, right: 40, bottom: 20, left: 40},
-    width =  750 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
-
     var parseDate = d3.time.format("%Y-%m-%dT%H:%M:%S%Z").parse;
 
-    var x = d3.time.scale()
-        .range([0, width]);
-
-    var y = d3.scale.linear()
-        .range([height, 0]);
-
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
-
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left");
-
-    var line = d3.svg.line()
-        .x(function(d) { return x(d.created_on); })
-        .y(function(d) { return y(d.total); });
-/*
-    var chart = d3.select(".chartPublic")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-*/
     var data = PublicProjectService.query(function() {
       var sum = 0;
       data.forEach(function(d) {
         d.created_on = parseDate(d.created_on);
         d.total = sum++;
       });
-      console.log(data);
-      
+      var graphData = [
+        {
+          key: 'Public Projects Created',
+          values: data
+        }
+      ];
+
       nv.addGraph(function() {
-	  var chart = nv.models.lineChart();
-	  chart.x(function(d) {
-	    return d.created_on;
-	  });
-	  chart.y(function(d) {
-	    return d.total;
-	  });
+        var chart = nv.models.lineChart();
+          chart.x(function(d) {
+            return d.created_on;
+          });
+          chart.y(function(d) {
+            return d.total;
+        });
 
-	  chart.xAxis
-	    .tickFormat(d3.format(',f'));
+        chart.xAxis.tickFormat(function(d) { return d3.time.format('%m/%Y')(new Date(d)) });
 
-	  chart.yAxis
-	    .tickFormat(d3.format(',f'));
+        chart.yAxis.tickFormat(d3.format(',f'));
 
-//	  chart.y2Axis
-//	    .tickFormat(d3.format(',f'));
+        chart.xScale(d3.time.scale());
 
-	  d3.select('.chartPublic')
-	    .datum(data)
-	    .transition().duration(500)
-	    .call(chart)
-	    ;
+      // chart.y2Axis
+      // .tickFormat(d3.format(',f'));
 
-	  nv.utils.windowResize(chart.update);
+        d3.select('.chartPublic')
+          .datum(graphData)
+          .transition().duration(500)
+          .call(chart);
 
-	  return chart;
-	});
+        nv.utils.windowResize(chart.update);
 
-/*      
-      x.domain(d3.extent($scope.projects, function(d) { return d.created_on; }));
-      y.domain(d3.extent($scope.projects, function(d) { return d.total; }));
+        return chart;
+      });
 
-      chart.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + height + ")")
-          .call(xAxis);
-
-      chart.append("g")
-          .attr("class", "y axis")
-          .call(yAxis)
-        .append("text")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 6)
-          .attr("dy", ".9em")
-          .style("text-anchor", "end")
-          .text("Total Projects");
-
-      chart.append("path")
-        .datum($scope.projects)
-        .attr("class", "line")
-        .attr("d", line);
-*/
     });
-
-    function type(d) {
-      d.value = +d.value; // coerce to number
-      return d;
-    }
 
   }])
   .controller('PrivateProjectsCtrl', [ '$scope', 'PrivateProjectService', function($scope, PrivateProjectService) {
-
-    var margin = {top: 20, right: 40, bottom: 20, left: 40},
-    width =  750 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
-
     var parseDate = d3.time.format("%Y-%m-%dT%H:%M:%S%Z").parse;
 
-    var x = d3.time.scale()
-        .range([0, width]);
-
-    var y = d3.scale.linear()
-        .range([height, 0]);
-
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
-
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left");
-
-    var line = d3.svg.line()
-        .x(function(d) { return x(d.created_on); })
-        .y(function(d) { return y(d.total); });
-
-    var chart = d3.select(".chartPrivate")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    $scope.projects = PrivateProjectService.query(function() {
+    var data = PrivateProjectService.query(function() {
       var sum = 0;
-      $scope.projects.forEach(function(d) {
-//        console.log(d);
+      data.forEach(function(d) {
         d.created_on = parseDate(d.created_on);
         d.total = sum++;
-      })
-      x.domain(d3.extent($scope.projects, function(d) { return d.created_on; }));
-      y.domain(d3.extent($scope.projects, function(d) { return d.total; }));
+      });
+      var graphData = [
+        {
+          key: 'Private Projects Created',
+          values: data
+        }
+      ];
 
-      chart.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + height + ")")
-          .call(xAxis);
+      nv.addGraph(function() {
+        var chart = nv.models.lineChart();
+          chart.x(function(d) {
+            return d.created_on;
+          });
+          chart.y(function(d) {
+            return d.total;
+        });
 
-      chart.append("g")
-          .attr("class", "y axis")
-          .call(yAxis)
-        .append("text")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 6)
-          .attr("dy", ".9em")
-          .style("text-anchor", "end")
-          .text("Total Projects");
+        chart.xAxis.tickFormat(function(d) { return d3.time.format('%m/%Y')(new Date(d)) });
 
-      chart.append("path")
-        .datum($scope.projects)
-        .attr("class", "line")
-        .attr("d", line);
+        chart.yAxis.tickFormat(d3.format(',f'));
+
+        chart.xScale(d3.time.scale());
+
+      // chart.y2Axis
+      // .tickFormat(d3.format(',f'));
+
+        d3.select('.chartPrivate')
+          .datum(graphData)
+          .transition().duration(500)
+          .call(chart);
+
+        nv.utils.windowResize(chart.update);
+
+        return chart;
+      });
+
     });
-
-    function type(d) {
-      d.value = +d.value; // coerce to number
-      return d;
-    }
 
   }])
   ;
